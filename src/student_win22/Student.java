4a64e1119b4365e20213a86578daab976c41da1c
TechExperiments <97964056+TechExperiments@users.noreply.github.com>
2022-01-25 09:43:29 -0500

    Update Student.java

--- src/student_win22/Student.java
+++ src/student_win22/Student.java
@@ -2,6 +2,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  * This code was written by Camilo Plaza
+ * Date: winter semester january 22
  */
 package student_win22;
 
